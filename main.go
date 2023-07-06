29c7fd72e7372dfbead0b66e734ba449601b6013
Ivan Kovalenko <ivan.kovalenko@s1ckret.com>
2023-07-06 23:04:43 +0300

    feat(#3): add AWS lambda proxy
    Signed-off-by: Ivan Kovalenko <ivan.kovalenko@s1ckret.com>

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
-func main() {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	return r
+}
+
+var ginLambda *ginadapter.GinLambda
+
+func main() {
+	r := setupRouter()
+	env := os.Getenv("GIN_MODE")
+	if env == "release" {
+		ginLambda = ginadapter.New(r)
+		lambda.Start(Handler)
+	} else {
+		r.Run(":8080")
+	}
+}
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, request)
 }
