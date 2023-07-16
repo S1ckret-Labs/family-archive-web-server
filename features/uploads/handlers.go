bbff975060b21ae0603f7997636e8b943adfa623
Ivan Kovalenko <ivan.kovalenko@s1ckret.com>
2023-07-16 19:54:06 +0300

    feat(#6, uploads): implement create requirement
    Signed-off-by: Ivan Kovalenko <ivan.kovalenko@s1ckret.com>

--- features/uploads/handlers.go
+++ features/uploads/handlers.go
@@ -18,6 +18,12 @@ type Feature struct {
 	BucketName string
 }
 
+type CreateUploadRequestResult struct {
+	ObjectId  int64
+	ObjectKey string
+	UploadUrl string
+}
+
 func (f Feature) GetUploadRequests(c *gin.Context) {
 	userId, err := helpers.ParamInt64(c, "id")
 	if err != nil {
@@ -40,7 +46,8 @@ func (f Feature) CreateUploadRequests(c *gin.Context) {
 		return
 	}
 
-	// TODO: validate file names
+	// TODO: Validate if file names are not colliding (must be unique)
+	// TODO: Validate file extensions (must be acceptable)
 	var fileNames []string
 	if err := c.BindJSON(&fileNames); err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
@@ -60,12 +67,25 @@ func (f Feature) CreateUploadRequests(c *gin.Context) {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	log.Println("Inserted UploadFiles: ", ids)
 
-	filesToUploadUrls := helpers.ZipToMap(fileNames, urls)
+	results := composeCreateUploadRequestResults(ids, fileNames, urls)
+	c.JSON(http.StatusOK, results)
+}
 
-	// Result
-	c.JSON(http.StatusOK, filesToUploadUrls)
+func composeCreateUploadRequestResults(ids []int64, keys []string, urls []string) []CreateUploadRequestResult {
+	if len(ids) != len(keys) || len(keys) != len(urls) {
+		log.Panicf("Can't compose final result. Array sizes doesn't match! %d, %d, %d\n", len(ids), len(keys), len(urls))
+	}
+
+	var results []CreateUploadRequestResult
+	for i, _ := range ids {
+		results = append(results, CreateUploadRequestResult{
+			ObjectId:  ids[i],
+			ObjectKey: keys[i],
+			UploadUrl: urls[i],
+		})
+	}
+	return results
 }
 
 func (f Feature) createS3SignedUrls(prefix string, fileNames []string) ([]string, error) {
